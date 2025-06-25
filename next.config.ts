53a675c181ffb214ebf5db44393b283e054719e7
mobisa-012 <mobisa12ivy@gmail.com>
2025-06-25 15:26:52 +0300

    next configuration to allow building the web

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     domains: ['miro.medium.com', 'cdn-images-1.medium.com', 'medium.com'],
   },
+  output: 'export',
+  trailingSlash: true,
 };
 
 export default nextConfig;
